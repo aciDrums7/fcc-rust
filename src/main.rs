ac5101f04b1fee6e6bda58ea28bef48c9bce1489
aciDrums7 <edoardo.carradori.96@gmail.com>
2024-03-04 22:00:42 +0000

    Added prettier configs + Updated vscode configs + Numbers & Binary System

--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,139 @@
-//* The main() fn is the starting point of every rust program
+//? If we don't explicitly assign a type to a variable, then the compiler will infer one for us.
+#![allow(unused)]
 fn main() {
-    destructuring_assignments();
+    computations();
 }
 
-fn uninitialized_error() {
-    let x: i32; // Uninitialized but used, ERROR !
-    let y: i32; // Uninitialized but also unused, only a Warning !
-    let x: i32 = 5; // Now it works properly
-
-    assert_eq!(x, 5);
-    println!("uninitialized_error() is working");
+fn type_of<T>(_: &T) -> String {
+    format!("{}", std::any::type_name::<T>())
 }
 
-fn mutable() {
-    //! In Rust, by default, a variable is immutable! You have to declare it mutable using the keyword "mut"
-    let mut x = 1;
-    x += 2;
-
-    assert_eq!(x, 3);
-    println!("mutable() is working");
-}
-
-fn scope() {
-    let x: i32 = 10;
-    {
-        //^ Since y is defined in an inner scope, the println! outside of this scope will throw error
-        let y: i32 = 5; // not working outside of this scope
-        println!("The value of x is {} and value of y is {}", x, y);
-    }
-    let y: i32 = 5; // now is working
-    println!("The value of x is {} and value of y is {}", x, y);
-}
-
-fn define_x() {
-    let x: &str = "hello";
-    println!("{}, world", x);
-}
-
-fn shadowing() {
-    // The first variable is "shadowed" by the second one
+fn different_types_unreassignables() {
     let x: i32 = 5;
-    {
-        let x = 12;
-        assert_eq!(x, 12);
+    // let mut y: u32 = 5;
+    let mut y = 5;
+
+    // We can't assign a var of a type another type
+    y = x;
+
+    let z = 10;
+
+    println!("different_types_unreassignables()")
+}
+
+fn annotating_values() {
+    let v: u16 = 38_u8 as u16;
+
+    println!("annotating_values()")
+}
+
+fn types_checks() {
+    let x: i32 = 5;
+    // This will throw a validation error
+    // assert_eq!("u32".to_string(), type_of(&x));
+
+    assert_eq!("i32".to_string(), type_of(&x));
+
+    println!("types_checks()");
+}
+
+fn max() {
+    assert_eq!(u8::MAX, 255);
+    assert_eq!(i8::MAX, 127);
+
+    println!("Success!");
+}
+
+fn inferred() {
+    // Overflow
+    // let var1: u8 = 251_u8 + 8_u8;
+    let var1: u16 = 251_u16 + 8;
+    // This expression will panic
+    // let var2 = i8::checked_add(251, 8).unwrap();
+    let var2 = i16::checked_add(251, 2).unwrap();
+    println!("{},{}", var1, var2);
+}
+
+fn numbers_representations() {
+    // ? In order: decimal, hexadecimal, octal and binary
+    let v = 1_024 + 0xff + 0o77 + 0b1111_1111; // 1024 + 255 + 63 + 255
+    assert!(v == 1597);
+
+    print!("numbers_representations()");
+}
+
+fn floating_point() {
+    let x = 1_000.000_1;
+    let y: f32 = 0.12;
+    let z = 0.01_f64;
+
+    assert_eq!(type_of(&x), "f64".to_string());
+    println!("floating_point()");
+}
+
+fn floating_point_precision() {
+    // This assertion will fail because of the extreme precision of f64
+    // assert!(0.1+0.2==0.3); // 0.1 + 0.2 ~= 0.3000000000002
+
+    // Instead, this will work
+    assert!((0.1 as f32) + (0.2 as f32) == (0.3 as f32));
+
+    print!("floating_point");
+}
+
+fn for_cycle() {
+    let mut sum: i32 = 0;
+
+    for i in -3..=2 {
+        sum += i;
     }
 
-    assert_eq!(x, 5);
+    assert!(sum == -3);
 
-    let x = 42;
-    println!("{}", x); // Prints "42"
+    // Printing the ASCII base 10 corrispondent to each char
+    for c in 'a'..='z' {
+        print!("{} ", c as u8);
+    }
 }
 
-fn shadowing_and_rebinding() {
-    let mut x: i32 = 1;
-    x = 7;
-    // let x = x; //Shadowing and re-binding will throw an error here, because vars in Rust are immutable by nature
-    x += 3;
+fn ranges() {
+    // These are more verbose ways to express a range
+    use std::ops::{ Range, RangeInclusive };
+    assert_eq!(1..5, Range { start: 1, end: 5 });
+    assert_eq!(1..=5, RangeInclusive::new(1, 5));
 
-    let y = 4;
-    // Shadowing
-    let y = "I can also be bound to text!";
-
-    println!("shadowing_and_rebinding() working");
+    println!("ranges()");
 }
 
-#[allow(unused_variables)]
-fn remove_warnings() {
-    let _x = 1; // Prepending with _ or using the "allow[]" will remove unused_variables warninng
-}
+fn computations() {
+    assert!(1u32 + 2 == 3);
 
-fn tuples_destructuring() {
-    let (mut x, y) = (1, 2);
-    x += 2;
+    assert!(1i32 - 2 == -1);
+    // assert!(1u8 - 2 == -1);
+    assert!(1i8 - 2 == -1);
 
-    assert_eq!(x, 3);
-    assert_eq!(y, 2);
+    assert!(3 * 50 == 150);
 
-    println!("tuples_destructuring() is working");
-}
+    assert!(9.6_f32 / 3.2_f32 == 3.0_f32);
 
-fn destructuring_assignments() {
-    let (x, y);
-    (x, ..) = (3, 4); // ".." means we don't care of the value at that index, so ignore it
-    [.., y] = [1, 2];
-    assert_eq!([x, y], [3, 2]);
+    assert!(24 % 5 == 4);
 
-    println!("destructuring_assignments() working")
+    // Short-circuiting boolean logic
+    assert!(true && false == false);
+    assert!(true || false == true);
+    assert!(!true == false);
+
+    // Bitwise operations
+    //^ each operation is made bit by bit (0 == false, 1 == true) -> bitwise! For instance:
+    //^ 0 0 1 1 AND
+    //^ 0 1 0 1
+    //^ -------
+    //^ 0 0 0 1
+    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
+    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
+    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
+    //^ 0 0 0 0 0 0 0 1 shifting 5 positions to left becomes
+    //^ 0 0 1 0 0 0 0 0 == 32 in decimal
+    println!("1 << 5 is {}", 1u32 << 5);
+    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
 }
